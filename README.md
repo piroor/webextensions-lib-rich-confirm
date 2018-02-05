572ca1b38484b0c27c463c5eb9fb4b32fdc64029
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2018-02-05 23:46:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# webextensions-lib-rich-confirm
+
+Helps to provide confirmation dialog with checkbox.
+
+## Required permissions
+
+ * `tabs` or `activeTab`, if you want to use `RichConfirm.show()`.
+
+## Basic usage
+
+Load the file `RichConfirm.js` from any document (background page, sidebar panel, or browser action panel), like:
+
+```json
+<script type="application/javascript" src="./RichConfirm.js"></script>
+```
+
+And, call `RichConfirm.show()` with required parameters like:
+
+```javascript
+var result = await RichConfirm.show({
+  message:      'Are you ready?',
+  buttons:      ['Yes', 'No'],
+  checkMessage: 'Never show',
+  checked:      false
+});
+```
+
+Here is the list of parameters:
+
+ * `message` (required): A message for the confirmation dialog. (`String`)
+ * `buttons` (required): Labels for buttons. (`Array` of `String`s)
+ * `checkMessage` (optional): A label for the checkbox. (`String`)
+ * `checked` (optional): Default state of the checkbox. (`Boolean`)
+
+`RichConfirm.show()` returns a `Promise`. It will be resolved with an object with following attributes:
+
+ * `buttonIndex`: The index of the button which is clicked. `-1` if the confirmation dialog is dismissed.
+ * `checked`: The state of the checkbox.
+
+## Confirmation in the content area
+
+If you want to show the confirmation dialog in the content area, call `RichConfirm.showInTab()` with an ID of a tab, like:
+
+```javascript
+var result = await RichConfirm.showInTab(10, {
+  message:      'Are you ready?',
+  buttons:      ['Yes', 'No'],
+  checkMessage: 'Never show',
+  checked:      false
+});
+```
+
+The first parameter is `tabs.Tab.id`, the second parameter is same to `RichConfirm.show()`.
+
---
+++ RichConfirm.js
@@ -0,0 +1,252 @@
+/*
+ license: The MIT License, Copyright (c) 2018 YUKI "Piro" Hiroshi
+ original:
+   https://github.com/piroor/webextensions-lib-rich-confirm
+*/
+'use strict';
+
+(function defineRichConfirm() {
+  const RichConfirm = function(aParams) {
+    this.params = aParams;
+    if (!this.params.buttons)
+      this.params.buttons = ['OK'];
+    this.onClick = this.onClick.bind(this);
+  }
+  RichConfirm.prototype = {
+    uniqueKey: parseInt(Math.random() * Math.pow(2, 16)),
+    get dialog() {
+      return this.ui.querySelector(`.rich-confirm-dialog${this.uniqueKey}`);
+    },
+    get message() {
+      return this.ui.querySelector(`.rich-confirm-message${this.uniqueKey}`);
+    },
+    get buttonsContainer() {
+      return this.ui.querySelector(`.rich-confirm-buttons${this.uniqueKey}`);
+    },
+    get checkContainer() {
+      return this.ui.querySelector(`.rich-confirm-check-label${this.uniqueKey}`);
+    },
+    get checkCheckbox() {
+      return this.ui.querySelector(`.rich-confirm-check-checkbox${this.uniqueKey}`);
+    },
+    get checkMessage() {
+      return this.ui.querySelector(`.rich-confirm-check-message${this.uniqueKey}`);
+    },
+
+    buildUI() {
+      this.style = document.createElement('style');
+      this.style.setAttribute('type', 'text/css');
+      this.style.textContent = `
+        .rich-confirm${this.uniqueKey},
+        .rich-confirm-row${this.uniqueKey} {
+          align-items: center;
+          bottom: 0;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          left: 0;
+          position: fixed;
+          right: 0;
+          top: 0;
+        }
+
+        .rich-confirm${this.uniqueKey} {
+          background: rgba(0, 0, 0, 0.45);
+          left:0;
+          opacity: 0;
+          pointer-events: none;
+          transition: opacity 250ms ease-out;
+          z-index: 999997;
+        }
+
+        .rich-confirm${this.uniqueKey}.show {
+          opacity: 1;
+          pointer-events: auto;
+        }
+
+        .rich-confirm-row${this.uniqueKey} {
+          z-index: 999998;
+        }
+
+        .rich-confirm-dialog${this.uniqueKey} {
+          background: -moz-dialog;
+          border-radius: 0.5em;
+          box-shadow: 0.1em 0.1em 0.5em rgba(0, 0, 0, 0.65);
+          color: -moz-dialogtext;
+          margin: 0.5em;
+          max-width: 20em;
+          padding: 1em;
+          z-index: 999999;
+        }
+
+        .rich-confirm-buttons${this.uniqueKey} {
+          align-items: stretch;
+          flex-direction: column;
+          justify-content: center;
+          margin: 0.5em 0 0;
+        }
+
+        .rich-confirm-buttons${this.uniqueKey} button {
+          display: block;
+          width: 100%;
+        }
+
+        .rich-confirm-check-label${this.uniqueKey} {
+          display: flex;
+          flex-direction: row;
+          margin-top: 0.5em;
+        }
+
+        .rich-confirm-check-label${this.uniqueKey}.hidden {
+          display: none;
+        }
+      `;
+      document.head.appendChild(this.style);
+
+      var range = document.createRange();
+      range.selectNodeContents(document.body);
+      range.collapse(false);
+      var fragment = range.createContextualFragment(`
+        <div class="rich-confirm${this.uniqueKey}">
+          <div class="rich-confirm-row${this.uniqueKey}">
+            <div class="rich-confirm-dialog${this.uniqueKey}">
+              <span class="rich-confirm-message${this.uniqueKey}"></span>
+              <div class="rich-confirm-buttons${this.uniqueKey}"></div>
+              <label class="rich-confirm-check-label${this.uniqueKey}">
+                <input type="checkbox"
+                       class="rich-confirm-check-checkbox${this.uniqueKey}">
+                <span class="rich-confirm-check-message${this.uniqueKey}"></span>
+              </label>
+            </div>
+          </div>
+        </div>
+      `);
+      range.insertNode(fragment);
+      range.detach();
+      this.ui = document.body.lastElementChild;
+    },
+
+    show: async function() {
+      this.buildUI();
+      await new Promise((aResolve, aReject) => setTimeout(aResolve, 0));
+
+      this.message.textContent = this.params.message;
+
+      if (this.params.checkMessage) {
+        this.checkMessage.textContent = this.params.checkMessage;
+        this.checkCheckbox.checked = !!this.params.checked;
+        this.checkContainer.classList.remove('hidden');
+      }
+      else {
+        this.checkContainer.classList.add('hidden');
+      }
+
+      var range = document.createRange();
+      range.selectNodeContents(this.buttonsContainer);
+      range.deleteContents();
+      var buttons = document.createDocumentFragment();
+      for (let label of this.params.buttons) {
+        let button = document.createElement('button');
+        button.textContent = label;
+        button.setAttribute('title', label);
+        buttons.appendChild(button);
+      }
+      range.insertNode(buttons);
+      range.detach();
+
+      this.ui.addEventListener('click', this.onClick);
+      this.ui.classList.add('show');
+
+      return new Promise((aResolve, aReject) => {
+        this._resolve = aResolve;
+        this._rejecte = aReject;
+      });
+    },
+
+    hide() {
+      this.ui.removeEventListener('click', this.onClick);
+      delete this._resolve;
+      delete this._rejecte;
+      this.ui.classList.remove('show');
+      window.setTimeout(() => {
+        this.ui.parentNode.removeChild(this.ui);
+        this.style.parentNode.removeChild(this.style);
+        delete this.ui;
+        delete this.style;
+      }, 1000);
+    },
+
+    onClick(aEvent) {
+      if (aEvent.button != 0) {
+        aEvent.stopPropagation();
+        aEvent.preventDefault();
+        return;
+      }
+
+      var button = aEvent.target.closest('button');
+      if (button) {
+        aEvent.stopPropagation();
+        aEvent.preventDefault();
+        let buttonIndex = Array.slice(this.buttonsContainer.childNodes).indexOf(button);
+        this._resolve({
+          buttonIndex,
+          checked: !!this.params.message && this.checkCheckbox.checked
+        });
+        this.hide();
+        return;
+      }
+
+      if (!aEvent.target.closest(`.rich-confirm-dialog`)) {
+        aEvent.stopPropagation();
+        aEvent.preventDefault();
+        this._resolve({
+          buttonIndex: -1,
+          checked: !!this.params.message && this.checkCheckbox.checked
+        });
+        this.hide();
+      }
+    }
+  };
+  RichConfirm.show = async function(aParams) {
+    const confirm = new this(aParams);
+    return confirm.show();
+  };
+  RichConfirm.showInTab = async function(aTabId, aParams) {
+    await browser.tabs.executeScript(aTabId, {
+      code: `
+        if (!window.RichConfirm)
+           (${defineRichConfirm.toSource()})();
+      `,
+      matchAboutBlank: true,
+      runAt:           'document_start'
+    });
+    browser.tabs.executeScript(aTabId, {
+      code: `
+      delete window.RichConfirm.result;
+      (async () => {
+        const confirm = new RichConfirm(${JSON.stringify(aParams)});
+        window.RichConfirm.result = await confirm.show();
+      })();
+    `,
+      matchAboutBlank: true,
+      runAt:           'document_start'
+    });
+    let result;
+    while (true) {
+      const results = await browser.tabs.executeScript(aTabId, {
+        code:            `window.RichConfirm.result`,
+        matchAboutBlank: true,
+        runAt:           'document_start'
+      });
+      if (results.length > 0 &&
+        results[0] !== undefined) {
+        result = results[0];
+        break;
+      }
+      await new Promise((aResolve, aReject) => setTimeout(aResolve, 100));
+    }
+    return result;
+  };
+  window.RichConfirm = RichConfirm;
+  return true; // this is required to run this script as a content script
+})();
