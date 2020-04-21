eeb137216958271d1ae46178e2e0112652874ddf
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-04-22 01:34:12 +0900

    Fix indent

--- RichConfirm.js
+++ RichConfirm.js
@@ -591,22 +591,22 @@
       // Step 2:
       // Resize the window to expected size on outside of the screen.
       if (/mac/i.test(navigator.platform)) {
-      // On macOS environment this operation must be done separately before
-      // window move, because resizing of a window outside the visible area
-      // moves the window into the main screen unexpectedly.
-      await browser.windows.update(win.id, {
-        width:  ownerWin.width,
-        height: ownerWin.height
-      });
-      // Step 2.5:
-      // Move the window outside the visible area, until all UI elements are
-      // prepared.
-      // The coordinates must be positive integer because large negative
-      // coordinates don't work as expected on macOS.
-      await browser.windows.update(win.id, {
-        top:  window.screen.height * 100,
-        left: window.screen.width * 100
-      });
+        // On macOS environment this operation must be done separately before
+        // window move, because resizing of a window outside the visible area
+        // moves the window into the main screen unexpectedly.
+        await browser.windows.update(win.id, {
+          width:  ownerWin.width,
+          height: ownerWin.height
+        });
+        // Step 2.5:
+        // Move the window outside the visible area, until all UI elements are
+        // prepared.
+        // The coordinates must be positive integer because large negative
+        // coordinates don't work as expected on macOS.
+        await browser.windows.update(win.id, {
+          top:  window.screen.height * 100,
+          left: window.screen.width * 100
+        });
       }
       else {
         await browser.windows.update(win.id, {
